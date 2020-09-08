e00aced41c3cbd3a6c5fa5a50ce4c35115a7b41c
Jordan Davis <44381603+jordandavis-asurion@users.noreply.github.com>
2020-09-08 14:36:09 -0500

    Update dart@2.rb

--- dart@2.rb
+++ dart@2.rb
@@ -11,17 +11,6 @@ class Dart < Formula
     sha256 "78a2da74ea83ee092463a9901467492ef885f6e378353b0a44481fdf40ea81c7"
   end
 
-  devel do
-    version "2.2.0-dev.2.1"
-    if Hardware::CPU.is_64_bit?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.2.0-dev.2.1/sdk/dartsdk-macos-x64-release.zip"
-      sha256 "7e1b3b94eb67dc2a40a7a45e28d1a2058cd219be5e62de700d82dc628af98eaf"
-    else
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.2.0-dev.2.1/sdk/dartsdk-macos-ia32-release.zip"
-      sha256 "ae0456a0758a4ec7c340ce2f77d1e1bdc1bd6daec014946e40c0a53b819f44a8"
-    end
-  end
-
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/dart"
